606e6cbd8f5cb6ace93d917b4040918fb8739a66
Tracy <safrant@nih.gov>
2023-08-02 10:49:15 -0400

    Update ivy.xml

--- lbImpl/src/org/LexGrid/LexBIG/Impl/LexEVSVersion.java
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/LexEVSVersion.java
@@ -10,8 +10,8 @@ package org.LexGrid.LexBIG.Impl;
  * and timestamp are updated with the current values.
  */
 public class LexEVSVersion {
-    private static final String VERSION = "6.6.1.RC2";
-    private static final String TIMESTAMP = "2023-03-23_11:01:40";
+    private static final String VERSION = "6.6.3.SNAPSHOT.1";
+    private static final String TIMESTAMP = "2023-08-01_13:42:16";
     
     public static String getLexEVSBuildVersion(){
         return VERSION;
