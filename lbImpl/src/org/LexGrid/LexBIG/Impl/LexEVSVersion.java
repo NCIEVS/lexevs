0f3e1f7c8a5996bd0eb1d153d868c70faac36f8a
Tracy <safrant@nih.gov>
2023-08-07 10:02:25 -0400

    update/LEXEVS-5355
    Release candidate 1 of Spring 5.3.29 update

--- lbImpl/src/org/LexGrid/LexBIG/Impl/LexEVSVersion.java
+++ lbImpl/src/org/LexGrid/LexBIG/Impl/LexEVSVersion.java
@@ -10,8 +10,8 @@ package org.LexGrid.LexBIG.Impl;
  * and timestamp are updated with the current values.
  */
 public class LexEVSVersion {
-    private static final String VERSION = "6.6.3-SNAPSHOT.3";
-    private static final String TIMESTAMP = "2023-08-03_10:08:26";
+    private static final String VERSION = "6.6.3-RC1";
+    private static final String TIMESTAMP = "2023-08-07_09:59:52";
     
     public static String getLexEVSBuildVersion(){
         return VERSION;
